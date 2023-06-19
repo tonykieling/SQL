d1649dfd9890cbdf24a7bb6a8bc43f57ba01ae15
Tony Kieling <tony.kieling@gmail.com>
2023-06-19 15:59:32 -0700

    practicing SQL

---
+++ code01.sql
@@ -0,0 +1,3 @@
+-- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+-- The CITY table is described as follows:
+select * from city where population > 100000 AND countrycode = 'USA';
---
+++ code07.sql
@@ -0,0 +1,3 @@
+-- Query a list of CITY and STATE from the STATION table.
+-- The STATION table is described as follows:
+select city, state from station;
